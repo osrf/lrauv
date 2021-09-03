7ea381917a4d2be4f2124cba8af25cf05a5b8917
Arjo Chakravarty <arjo@openrobotics.org>
2021-09-03 14:17:36 +0800

    Add nescesary files
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

---
+++ lrauv_ignition_plugins/src/AcousticCommsPlugin.hh
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* Development of this module has been funded by the Monterey Bay Aquarium
+Research Institute (MBARI) and the David and Lucile Packard Foundation */
+
+#ifndef TETHYS_BUOYANCY_ENGINE_
+#define TETHYS_BUOYANCY_ENGINE_
+
+#include <ignition/gazebo/System.hh>
+
+namespace tethys
+{
+
+class AcousticCommsPrivateData;
+
+class AcousticCommsPlugin:
+  public ignition::gazebo::System,
+  public ignition::gazebo::ISystemConfigure,
+  public ignition::gazebo::ISystemPreUpdate
+{
+  public: AcousticCommsPlugin();
+
+  /// Inherits documentation from parent class
+  public: void Configure(
+      const ignition::gazebo::Entity &_entity,
+      const std::shared_ptr<const sdf::Element> &_sdf,
+      ignition::gazebo::EntityComponentManager &_ecm,
+      ignition::gazebo::EventManager &/*_eventMgr*/
+  );
+
+  /// Inherits documentation from parent class
+  public: void PreUpdate(
+      const ignition::gazebo::UpdateInfo &_info,
+      ignition::gazebo::EntityComponentManager &_ecm);
+
+  /// Inherits documentation from parent class
+  private: std::unique_ptr<AcousticCommsPrivateData> dataPtr;
+};
+}
+
+#endif
