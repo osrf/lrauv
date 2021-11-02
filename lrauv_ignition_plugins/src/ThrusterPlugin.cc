gazebosim/gz-sim

add manual code check scripts
Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

lrauv_ignition_plugins/src/ThrusterPlugin.cc
@@ -42,8 +42,8 @@ class ThrusterPrivateData
 };
 
 ThrusterPlugin::ThrusterPlugin()
+  : _data(std::make_unique<ThrusterPrivateData>())
 {
-    _data = std::make_unique<ThrusterPrivateData>();
 }
 
 ThrusterPlugin::~ThrusterPlugin()

osrf/lrauv

lrauv_ignition_plugins/src/ThrusterPlugin.cc
@@ -51,8 +51,8 @@ class ThrusterPrivateData
 };
 
 ThrusterPlugin::ThrusterPlugin()
+  : dataPtr(std::make_unique<ThrusterPrivateData>())
 {
-    this->dataPtr = std::make_unique<ThrusterPrivateData>();
 }
 
 ThrusterPlugin::~ThrusterPlugin()