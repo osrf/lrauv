36be8a024b720d92c9230136189603567f281b3d
Arjo Chakravarty <arjo@openrobotics.org>
2021-12-21 13:12:27 +0800

    Fix tolerances. :man-facepalming:
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- lrauv_ignition_plugins/test/test_mission_yoyo_circle.cc
+++ lrauv_ignition_plugins/test/test_mission_yoyo_circle.cc
@@ -116,9 +116,9 @@ TEST_F(LrauvTestFixture, YoYoCircle)
     if (i > 6000)
     {
       // Once the vehicle achieves its full velocity the vehicle should have a
-      // nominal yaw rate of around 0.37-0.38rad/s. This means that the vehicle
-      // should keep spinning in a circle.
-      EXPECT_NEAR(tethysAngularVel[i].Z(), 0.037, 2e-2)
+      // nominal yaw rate of around 0.037-0.038rad/s. This means that the
+      // vehicle should keep spinning in a circle.
+      EXPECT_NEAR(tethysAngularVel[i].Z(), 0.037, 2e-3)
         << i << " yaw rate: " << tethysAngularVel[i].Z();
 
       // At the same time the roll rate should be near zero
