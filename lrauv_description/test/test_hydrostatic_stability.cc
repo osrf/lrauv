dd4a7b04f566266e65fec09e5a5e691e6d543a36
Arjo Chakravarty <arjo@openrobotics.org>
2021-12-23 01:29:38 +0800

    Update lrauv_description/test/test_hydrostatic_stability.cc
    Co-authored-by: Louise Poubel <louise@openrobotics.org>

--- lrauv_description/test/test_hydrostatic_stability.cc
+++ lrauv_description/test/test_hydrostatic_stability.cc
@@ -113,7 +113,7 @@ TEST(Stability, TiltedWorld)
     if (first)
     {
       prev_pose = pose;
-      first = true;
+      first = false;
       continue;
     }
     EXPECT_NEAR(prev_pose.Pos().X(), pose.Pos().X(), 1e-2);
