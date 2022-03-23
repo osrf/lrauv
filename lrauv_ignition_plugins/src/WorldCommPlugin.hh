9a421528d6427800a088557f5746e465c1931a97
braanan <byraanan@mbari.org>
2022-03-23 09:21:19 -0700

    spawn vehicles with acomms address (#170)
    * add acomms modem address entry to lrauv_init.proto
    
    * add modify AcousticCommsPlugin and RangeBearingPlugin address fields on init
    
    * add set acomms address in spawn test init messages
    
    * add test expectation to intercept formulated acomms topic with specified modem ID

--- lrauv_ignition_plugins/src/WorldCommPlugin.hh
+++ lrauv_ignition_plugins/src/WorldCommPlugin.hh
@@ -53,10 +53,11 @@ namespace tethys
     public: void SpawnCallback(
                 const lrauv_ignition_plugins::msgs::LRAUVInit &_msg);
 
-    /// Get the SDF string for a Tethys model with the given ID.
-    /// \param[in] _id Used as model name and namespace for topics.
+    /// Get the SDF string for a Tethys model with given ID and acoustic modem address.
+    /// \param[in] _msg LRAUV init message containing ID and acoustic modem address.
     /// \return SDF string
-    public: std::string TethysSdfString(const std::string &_id);
+    public: std::string TethysSdfString(
+                const lrauv_ignition_plugins::msgs::LRAUVInit &_msg);
 
     /// Generic callback to handle service responses.
     /// \param[in] _rep Response
