c13ee545c1d0b050c18c358bef9736b16e8ff868
Michel Hidalgo <michel@ekumenlabs.com>
2022-04-19 17:56:07 -0300

    Add lrauv_system_tests package.
    Including support libraries for testing.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ lrauv_system_tests/src/Publisher.cc
@@ -0,0 +1,25 @@
+#include "lrauv_system_tests/Publisher.hh"
+
+#include <thread>
+
+#include "lrauv_system_tests/Util.hh"
+
+namespace lrauv_system_tests
+{
+
+bool WaitForConnections(
+  const ignition::transport::Node::Publisher &_publisher,
+  const std::chrono::nanoseconds _timeout)
+{
+  Timeout timeout{_timeout};
+  while (!_publisher.HasConnections() && !timeout)
+  {
+    using namespace std::literals::chrono_literals;
+    constexpr std::chrono::nanoseconds minSleepTime = 100ms;
+    std::this_thread::sleep_for(
+        std::min(timeout.TimeRemaining(), minSleepTime));
+  }
+  return _publisher.HasConnections();
+}
+
+}
