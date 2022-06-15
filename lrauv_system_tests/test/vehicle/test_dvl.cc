968ab6ba83a2ddafa3ac357e9dabfb3bed83745a
Michel Hidalgo <michel@ekumenlabs.com>
2022-06-15 19:02:59 -0300

    Add simple DopplerVelocityLog test
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ lrauv_system_tests/test/vehicle/test_dvl.cc
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * Development of this module has been funded by the Monterey Bay Aquarium
+ * Research Institute (MBARI) and the David and Lucile Packard Foundation
+ */
+
+#include <gtest/gtest.h>
+
+#include <gz/math/Vector3.hh>
+#include <gz/msgs/Utility.hh>
+#include <gz/transport/Node.hh>
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <lrauv_ignition_plugins/dvl_velocity_tracking.pb.h>
+#include <lrauv_ignition_plugins/dvl_tracking_target.pb.h>
+
+#include "lrauv_system_tests/TestFixture.hh"
+#include "lrauv_system_tests/Util.hh"
+
+#include "TestConstants.hh"
+
+using namespace lrauv_system_tests;
+using namespace std::literals::chrono_literals;
+
+//////////////////////////////////////////////////
+TEST(DVLTest, BottomTrackingWorks)
+{
+  constexpr double kTolerance{1e-2};
+
+  VehicleCommandTestFixture fixture("sea_bottom_world.sdf", "tethys");
+
+  using DVLVelocityTracking =
+      lrauv_ignition_plugins::msgs::DVLVelocityTracking;
+  Subscription<DVLVelocityTracking> velocitySubscription;
+  velocitySubscription.Subscribe(fixture.Node(), "/tethys/dvl/velocity", 1);
+
+  // Step a few iterations for simulation to setup itself
+  uint64_t iterations = fixture.Step(100u);
+  EXPECT_EQ(100u, iterations);
+
+  // Propel vehicle forward by giving the propeller a positive
+  // angular velocity. Vehicle is supposed to move at around
+  // 1 m/s with 300 RPM. 300 RPM = 300 * 2 pi / 60 = 10 pi rad/s
+  lrauv_ignition_plugins::msgs::LRAUVCommand command;
+  command.set_propomegaaction_(10. * IGN_PI);
+
+  // Rotate rudder clockwise when looking from the top,
+  // which causes the vehicle to move in a counter-clockwise arch
+  command.set_rudderangleaction_(0.8);
+
+  // Neutral buoyancy
+  command.set_dropweightstate_(true);
+  command.set_buoyancyaction_(0.0005);
+
+  // Step simulation for some time for DVL estimates to estabilize
+  auto & publisher = fixture.CommandPublisher();
+  for (int _ = 0; _ < 5; ++_)
+  {
+    publisher.Publish(command);
+    fixture.Step(10s);
+  }
+  ASSERT_TRUE(velocitySubscription.WaitForMessages(50, 10s));
+
+  using DVLTrackingTarget = lrauv_ignition_plugins::msgs::DVLTrackingTarget;
+  const DVLVelocityTracking message = velocitySubscription.ReadLastMessage();
+  ASSERT_TRUE(message.has_target());
+  EXPECT_EQ(message.target().type(), DVLTrackingTarget::DVL_TARGET_BOTTOM);
+  EXPECT_NEAR(message.target().range().mean(), 20., kTolerance);
+  for (int i = 0; i < message.beams_size(); ++i)
+  {
+    EXPECT_EQ(message.beams(i).id(), i + 1);
+    EXPECT_TRUE(message.beams(i).locked())
+        << "Beam #" << message.beams(i).id() << " not locked";
+    EXPECT_NEAR(message.beams(i).range().mean(), 20., kTolerance)
+        << "Beam #" << message.beams(i).id() << " range is off";
+  }
+  ASSERT_TRUE(message.has_velocity());
+  const gz::math::Vector3d linearVelocityEstimate =
+      gz::msgs::Convert(message.velocity().mean());
+  const auto &linearVelocities =
+      fixture.VehicleObserver().LinearVelocities();
+  const auto &poses = fixture.VehicleObserver().Poses();
+  // Linear velocities w.r.t. to sea bottom are reported
+  // in a sensor affixed, SFM frame.
+  const auto &sensorRotation = poses.back().Rot();
+  const gz::math::Vector3d expectedLinearVelocityEstimate =
+      sensorRotation.RotateVectorReverse(linearVelocities.back());
+  EXPECT_NEAR(linearVelocityEstimate.X(),
+              expectedLinearVelocityEstimate.X(),
+              kTolerance);
+  EXPECT_NEAR(linearVelocityEstimate.Y(),
+              expectedLinearVelocityEstimate.Y(),
+              kTolerance);
+  EXPECT_NEAR(linearVelocityEstimate.Z(),
+              expectedLinearVelocityEstimate.Z(),
+              kTolerance);
+}
